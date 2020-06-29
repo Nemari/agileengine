a1eff320b7a8e35149493cac3a22d4bb369131ce
Mariia Hrytsaienko <44789399+Nemari@users.noreply.github.com>
2020-06-29 20:03:29 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 py main.py https://agileengine.bitbucket
 .io/beKIvpUlPMtzhfAy/samples/sample-0-origin.html https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/s
 amples/sample-3-the-escape.html
-pause
