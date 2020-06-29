67afbf3a2f56910eaffaeb3ff7b086ad76e0aea8
Mariia Hrytsaienko <44789399+Nemari@users.noreply.github.com>
2020-06-29 19:58:21 +0300

    + output sample, run script

---
+++ run.bat
@@ -0,0 +1,4 @@
+py main.py https://agileengine.bitbucket
+.io/beKIvpUlPMtzhfAy/samples/sample-0-origin.html https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/s
+amples/sample-3-the-escape.html
+pause
